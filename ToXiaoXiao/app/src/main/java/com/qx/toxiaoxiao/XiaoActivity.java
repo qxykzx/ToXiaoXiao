2123f43a5d02e98c66bcac415da30852f80a3676
qxykzx <qxykzx@163.com>
2017-12-24 00:06:39 +0800

    CreateProject

---
+++ ToXiaoXiao/app/src/main/java/com/qx/toxiaoxiao/XiaoActivity.java
@@ -0,0 +1,13 @@
+package com.qx.toxiaoxiao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class XiaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xiao);
+    }
+}
